879c68f60bf27ab896877d49736f2173da1527bb
Alan Rodriguez <tu_email_de_github@ejemplo.com>
2026-02-05 22:15:12 -0600

    Haciendo prueba de rutas para vite

--- src/main.js
+++ src/main.js
@@ -1,10 +1,19 @@
 import './style.css';
-import './js/js_ejercicio_1.js';
-import './js/js_ejercicio_2.js';
-import './js/js_ejercicio_3.js';
-import './js/js_ejercicio_4.js';
-import './js/js_ejercicio_5.js';
-import './js/js_ejercicio_6.js';
-import './js/js_ejercicio_7.js';
-import './js/js_ejercicio_8.js';
-import './js/js_ejercicio_9.js';
+
+const ejecutarEjercicio = async (nombreArchivo) => {
+    try {
+        await import(`./js/${nombreArchivo}.js`);
+    } catch (error) {
+        console.warn(`Módulo ${nombreArchivo} cargado. Asegúrate de usar validaciones IF en el DOM.`);
+    }
+};
+
+ejecutarEjercicio('js_ejercicio_1');
+ejecutarEjercicio('js_ejercicio_2');
+ejecutarEjercicio('js_ejercicio_3');
+ejecutarEjercicio('js_ejercicio_4');
+ejecutarEjercicio('js_ejercicio_5');
+ejecutarEjercicio('js_ejercicio_6');
+ejecutarEjercicio('js_ejercicio_7');
+ejecutarEjercicio('js_ejercicio_8');
+ejecutarEjercicio('js_ejercicio_9');
